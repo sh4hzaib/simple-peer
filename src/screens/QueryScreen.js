124eb5d02aa30f875d7806cb7bc34e0d64cda07e
sultan-O7t9 <18094198-079@uog.edu.pk>
2021-08-28 11:57:07 +0500

    Added Back Button to QueryPage

--- src/screens/QueryScreen.js
+++ src/screens/QueryScreen.js
@@ -4,17 +4,19 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Text } from "react-native";
+import { TouchableOpacity } from "react-native";
 import { View } from "react-native";
 import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native";
-
-const QueryScreen = ({ data, setData }) => {
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+const QueryScreen = ({ navigation, data, setData }) => {
   const focused = useIsFocused();
+  const [loadData, setLoadData] = useState(true);
   useFocusEffect(
     React.useCallback(() => {
       console.log("Focused:", focused);
       const queryEverySec = () => {
-        if (focused == true) {
+        if (focused == true && loadData == true) {
           axios.get("http://meldre.tplinkdns.com:8080/getdevice").then((r) => {
             const tempD = [];
             for (const key in r.data) {
@@ -44,11 +46,38 @@ const QueryScreen = ({ data, setData }) => {
               style={{
                 width: "100%",
                 paddingVertical: 40,
-                paddingBottom: 20,
                 alignItems: "center",
+                justifyContent: "center",
               }}
             >
-              <Text style={{ fontSize: 36, fontWeight: "bold" }}>
+              <TouchableOpacity
+                style={{ position: "absolute", left: "5%" }}
+                onPress={() => {
+                  setLoadData(false);
+                  setData([]);
+                  navigation.navigate("SettingsScreen");
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    padding: 6,
+                    paddingVertical: 8,
+                    backgroundColor: "#eee",
+                    borderWidth: 4,
+                    borderColor: "#ccc",
+                    borderRadius: 12,
+                  }}
+                >
+                  <MaterialCommunityIcons
+                    name="chevron-left"
+                    color={"#000"}
+                    size={20}
+                  />
+                  <Text>Back</Text>
+                </View>
+              </TouchableOpacity>
+              <Text style={{ fontSize: 24, fontWeight: "bold" }}>
                 Querying Data
               </Text>
             </View>
